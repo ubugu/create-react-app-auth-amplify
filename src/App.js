1c3fba856b40b1d446f07f2f84a6e28006488c7a
Claudio Sanna <c.sanna@reply.it>
2021-12-30 23:38:32 +0100

    Aggiunto troll

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './giraffScroll.gif';
 import './App.css';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
@@ -11,17 +11,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+			GIRAFFINU
+          </h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+            The best NFT 10k edition is coming to your house!
+			          <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://derpcentral.weebly.com/troll-page.html"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          >Buy now </a>- Only 10000000$ each one
+          </p>
         </header>
       </div>
     );
